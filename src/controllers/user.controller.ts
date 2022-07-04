a89f295d7086bd471ac89228ad16d1d0adfd8749
Salvamar22 <salvamarhernandez@gmail.com>
2022-07-04 06:40:59 -0600

    :hammer: validations

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,17 +7,41 @@ import { Request, Response } from 'express';
 import { ICreateUserRequest } from '@interfaces/user.interfaces';
 import userModel from '@models/user.model';
 
+interface IError {
+    message: string;
+}
 //creamos las funciones
 export const createUser = async (req: ICreateUserRequest, res: Response) => {
     const { body } = req;
-
+    try{
 // Crearemos un nuevo usuario con el body 
+//logica de negocio
+const existinUser = await userModel.findOne({email: body.email })
+//fail fast 
+    if(existinUser)
+    throw { message : 'User already exist'};
+
+
     const newUser = new userModel(body);
     //guardaremos el nuevo usuario que crearemos
     await newUser.save();
 
     // Nos retornara la informacion que acabamos de crear
     return res.status(201).json(newUser);
+    }
+    catch(err: any) {
+        //logica personalizada para los errores
+        
+        return res.status(400).json(err);
+    }
+}
+
+export const getById = async (req: Request, res: Response) => {
+
+    //Extraemos la query
+    const { params } = req;
+    const user = await userModel.findOne({_id: params.id});
+    return res.status(user ? 200 : 404).json(user);
 }
 
 export const getAllUsers = async (req: Request, res: Response) => {
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createUser , getAllUsers } from 'src/controllers/user.controller';
+import { createUser , getAllUsers, getById } from 'src/controllers/user.controller';
 
 const userRouter = Router();
 
 //Crearemos un usuario 
 userRouter.post('/create', createUser);
+
+userRouter.get('/:id', getById);
 userRouter.get('/', getAllUsers);
 
 export default userRouter;
